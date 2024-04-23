94468fe68c27fa5f53e202de4bd38a248fb3e651
nathan lins <nathan.lins012@gmail.com>
2024-04-23 20:42:42 -0300

    add primitive types example

---
+++ class-01-data-types/main.py
@@ -0,0 +1,18 @@
+milk = "leite desnatado"
+volume = 2.5
+
+print(type(milk))
+print(type(volume))
+
+preparation1 = volume + 1
+preparation2 = milk + "10"
+
+print(preparation1)
+print(preparation2)
+
+life_cat = str (volume + 5)
+
+print("o gato bebe" + milk)
+print("E tem" + life_cat + "vidas")
+
+print(F"o gato bebe {milk} e tem {life_cat} vidas")
