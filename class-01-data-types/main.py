8d37d5dcce729ee5c8ad431be28a62570d7b4085
nathan lins <nathan.lins012@gmail.com>
2024-04-24 20:30:55 -0300

    add structural types examples

--- class-01-data-types/main.py
+++ class-01-data-types/main.py
@@ -1,3 +1,5 @@
+# Primitive Types
+
 milk = "leite desnatado"
 volume = 2.5
 
@@ -16,3 +18,59 @@ print("o gato bebe" + milk)
 print("E tem" + life_cat + "vidas")
 
 print(F"o gato bebe {milk} e tem {life_cat} vidas")
+print(F"o gato bebe {milk} e tem {life_cat} vidas")
+
+name = "Davi"
+age = 31
+is_admin = False #boolean True of False
+
+print(is_admin)
+print(type(is_admin))
+
+# Structural Types (Collection)
+
+animes = ["Dragon Ball", "Naruto", "Cavalheiros do Zodiaco", "Death Note"]
+          # => 0                1              2                      3
+          # <= -4              -3             -2                     -1
+
+print(animes)
+print(type(animes))
+print(animes[0])
+print(animes[3])
+print(animes[-1])
+
+list = ["A", 123, True, ["B", "C"]]
+
+print(list[0])
+print(list[3])
+print(list[-1])
+print(list[-1][1])
+
+list[2] = False
+list[-1][0] = "0"
+print(list)
+print(len(list)) # length
+
+# Tuple
+tuple = ("A", 123, True)
+print(type(tuple))
+print(tuple[1])
+
+tuple[1] = 798 # error: tuple is immutable 
+
+print(tuple)
+
+# dict
+
+dict = {
+    "name": "Jonatan",
+    "age": 19,
+    "is_admin": True
+}
+
+print(dict)
+print(type(dict))
+
+print(dict["name"])
+print(dict["age"])
+print(dict["is_admin"])
