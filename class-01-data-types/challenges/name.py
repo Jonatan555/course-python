c7592a1d6e09961b1d1ea138c0edd6c3e99c4106
nathan lins <nathan.lins012@gmail.com>
2024-04-25 19:42:15 -0300

    add challenge name

---
+++ class-01-data-types/challenges/name.py
@@ -0,0 +1,8 @@
+# Capture o nome do usuário via "input"
+# atribua a uma variavel
+# Imprima na tela uma mensagem de boas vindas com o nome
+
+name = input("Digite seu nome:") 
+print(["Bem-vindo(a), {name}!"])
+   
+
